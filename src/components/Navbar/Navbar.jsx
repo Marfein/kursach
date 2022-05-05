b68abee370376111c86621046ad9f95991cdc43c
Marfein <mitskevich10@mail.ru>
2022-05-05 18:37:23 +0900

    начал делать верстку

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import s from './Navbar.module.css';
 import {NavLink} from "react-router-dom";
+
+import s from './Navbar.module.css';
+
+
 
 
 const Navbar = () => {
@@ -14,9 +17,9 @@ const Navbar = () => {
             <input type="checkbox" id="menu_control" className={s.menu_control}/>
            <div className={s.nav}> <div className={s.items}>
                 <div className={s.item}><NavLink to="/stocks" className = { navData => navData.isActive ? s.active : s.ir }>Акции</NavLink></div>
-                <div className={s.item}> <NavLink to="/stocks" className = { navData => navData.isActive ? s.active : s.ir }>Новинки</NavLink></div>
+                <div className={s.item}> <NavLink to="/news" className = { navData => navData.isActive ? s.active : s.ir }>Новинки</NavLink></div>
                 <div className={s.item}> <NavLink to="/hot" className = { navData => navData.isActive ? s.active : s.ir }>Горячие напитки</NavLink></div>
-                <div className={s.item}> <NavLink to="/stocks" className = { navData => navData.isActive ? s.active : s.ir }>Холодные напитки</NavLink></div>
+                <div className={s.item}> <NavLink to="/cold" className = { navData => navData.isActive ? s.active : s.ir }>Холодные напитки</NavLink></div>
             </div>
             <label htmlFor="menu_control" className={s.nav_close}/>
            </div>
---
+++ src/components/Stocks/Stocks.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Stocks.modules.css';
+
+const Stocks = () => {
+    return (
+<div className="sale">
+    Акции
+</div>
+
+    );
+}
+
+export default Stocks;
